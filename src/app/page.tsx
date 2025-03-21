620962bbd1c7af2785c240383702a94a5ae583ae
De Chirico Francesco <fradc16@gmail.com>
2025-03-21 21:51:55 +0100

    main: setup proj

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    // <main className="flex-1 bg-gradient-to-b from-gray-900 to-black">
+    <main className="flex-1 ">
+      <div className="container  mx-auto px-4 py-8  h-screen  flex flex-col">
+        <h1 className="text-4xl font-bold mb-8">
+          Library
+        </h1>
+        ...content here...
+      </div>
+    </main>
+  );
+}
