2b46d31252e625297dffedf0919e3e19f0cf8196
De Chirico Francesco <fradc16@gmail.com>
2025-04-18 15:32:16 +0200

    backend: feat: kpis setting up

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KpisModule } from './kpis/kpis.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       // https://docs.nestjs.com/techniques/configuration#use-module-globally
       // isGlobal: true, 
-    }), 
+    }),
+    KpisModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
